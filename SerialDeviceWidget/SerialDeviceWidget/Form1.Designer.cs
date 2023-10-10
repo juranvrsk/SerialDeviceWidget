109b4dbbc21085186514e625a3715d3cd5c42437
juranvrsk <pim1289@yandex.ru>
2023-10-10 16:53:21 +0300

    Added the enumeration and refresh logic

--- SerialDeviceWidget/SerialDeviceWidget/Form1.Designer.cs
+++ SerialDeviceWidget/SerialDeviceWidget/Form1.Designer.cs
@@ -29,30 +29,31 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBox = new System.Windows.Forms.GroupBox();
             this.listViewSerial = new System.Windows.Forms.ListView();
             this.buttonCheckAll = new System.Windows.Forms.Button();
             this.buttonRefresh = new System.Windows.Forms.Button();
             this.buttonApply = new System.Windows.Forms.Button();
             this.notifyIconMain = new System.Windows.Forms.NotifyIcon(this.components);
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItemRefresh = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.groupBox1.SuspendLayout();
-            this.contextMenuStrip1.SuspendLayout();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.groupBox.SuspendLayout();
+            this.contextMenuStripMain.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // groupBox
             // 
-            this.groupBox1.Controls.Add(this.listViewSerial);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 397);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "List of serial devices";
+            this.groupBox.Controls.Add(this.listViewSerial);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(297, 397);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "List of serial devices";
             // 
             // listViewSerial
             // 
@@ -60,9 +61,10 @@
             this.listViewSerial.HideSelection = false;
             this.listViewSerial.Location = new System.Drawing.Point(6, 19);
             this.listViewSerial.Name = "listViewSerial";
-            this.listViewSerial.Size = new System.Drawing.Size(188, 372);
+            this.listViewSerial.Size = new System.Drawing.Size(285, 372);
             this.listViewSerial.TabIndex = 0;
             this.listViewSerial.UseCompatibleStateImageBehavior = false;
+            this.listViewSerial.View = System.Windows.Forms.View.List;
             // 
             // buttonCheckAll
             // 
@@ -81,6 +83,7 @@
             this.buttonRefresh.TabIndex = 3;
             this.buttonRefresh.Text = "Refresh";
             this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
             // 
             // buttonApply
             // 
@@ -93,72 +96,76 @@
             // 
             // notifyIconMain
             // 
+            this.notifyIconMain.ContextMenuStrip = this.contextMenuStripMain;
+            this.notifyIconMain.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconMain.Icon")));
             this.notifyIconMain.Text = "SerialDeviceWidget";
             this.notifyIconMain.Visible = true;
             // 
-            // contextMenuStrip1
+            // contextMenuStripMain
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItemRefresh,
-            this.toolStripSeparator1,
             this.toolStripSeparator2,
-            this.toolStripMenuItemExit});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 82);
+            this.toolStripMenuItemExit,
+            this.toolStripSeparator1});
+            this.contextMenuStripMain.Name = "contextMenuStrip1";
+            this.contextMenuStripMain.Size = new System.Drawing.Size(114, 60);
             // 
             // toolStripMenuItemRefresh
             // 
             this.toolStripMenuItemRefresh.Name = "toolStripMenuItemRefresh";
-            this.toolStripMenuItemRefresh.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemRefresh.Size = new System.Drawing.Size(113, 22);
             this.toolStripMenuItemRefresh.Text = "Refresh";
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripMenuItemRefresh.Click += new System.EventHandler(this.toolStripMenuItemRefresh_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(110, 6);
             // 
             // toolStripMenuItemExit
             // 
             this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
-            this.toolStripMenuItemExit.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(113, 22);
             this.toolStripMenuItemExit.Text = "Exit";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(110, 6);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(229, 451);
+            this.ClientSize = new System.Drawing.Size(321, 451);
             this.Controls.Add(this.buttonApply);
             this.Controls.Add(this.buttonRefresh);
             this.Controls.Add(this.buttonCheckAll);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "FormMain";
             this.Text = "SerialDevices";
-            this.groupBox1.ResumeLayout(false);
-            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox.ResumeLayout(false);
+            this.contextMenuStripMain.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox;
         private System.Windows.Forms.ListView listViewSerial;
         private System.Windows.Forms.Button buttonCheckAll;
         private System.Windows.Forms.Button buttonRefresh;
         private System.Windows.Forms.Button buttonApply;
         private System.Windows.Forms.NotifyIcon notifyIconMain;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMain;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemRefresh;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
     }
 }
 
--- SerialDeviceWidget/SerialDeviceWidget/Form1.cs
+++ SerialDeviceWidget/SerialDeviceWidget/Form1.cs
@@ -13,9 +13,77 @@ namespace SerialDeviceWidget
 {
     public partial class FormMain : Form
     {
+        public List<String> PortNames;
+
         public FormMain()
         {
             InitializeComponent();
+            PortNames = new List<string>();
+            EnumarateCOMPortsWMI();
         }
-    }
+
+        private void toolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripMenuItemRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshEnumeration();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshEnumeration();
+        }
+
+        private void RefreshEnumeration()
+        {
+            //Serial ports list clear
+            foreach (String portName in PortNames)
+            {
+                contextMenuStripMain.Items.RemoveByKey(portName);
+                listViewSerial.Items.RemoveByKey(portName);
+            }
+            PortNames.Clear();
+            EnumarateCOMPortsWMI();
+        }
+
+        private void EnumarateCOMPortsWMI()
+        {
+            try
+            {
+                //Choosing from the Win32 WMI list of the PnP devices, which contains the COM ports
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%(COM[0-9]%'");
+                //Add the numeration for port names
+                int portCounter = 0;
+                foreach (ManagementObject queryObj in searcher.Get())//Took all port names from query result
+                {
+                    PortNames.Add("AddedPort" + portCounter.ToString());
+                    string res = String.Format("{0}", queryObj["Caption"]);
+                    var portMenuItem = new ToolStripMenuItem(res);
+                    portMenuItem.Name = PortNames.Last();
+                    var portListViewItem = new ListViewItem(res);
+                    portListViewItem.Name = PortNames.Last();
+                    //portMenuItem.Click += PortMenuItem_Click;
+                    contextMenuStripMain.Items.Add(portMenuItem);
+                    listViewSerial.Items.Add(portListViewItem);
+                }
+            }
+            catch (ManagementException e) //In case of error output error message to devices list
+            {
+                //Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
+                var portMenuItem = new ToolStripMenuItem("An error occurred while querying for WMI data: " + e.Message);
+                contextMenuStripMain.Items.Add(portMenuItem);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            notifyIconMain.Visible = false;
+            base.OnFormClosing(e);
+        }
+
+      
+    }    
 }
