db5895977437ad96eb8744eb4bb2460d0f1d92a1
juranvrsk <pim1289@yandex.ru>
2023-10-18 14:55:22 +0300

    Refresh functions added

--- SerialDeviceWidget/SerialDeviceWidget/Form1.Designer.cs
+++ SerialDeviceWidget/SerialDeviceWidget/Form1.Designer.cs
@@ -39,10 +39,16 @@
             this.contextMenuStripMain = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.toolStripMenuItemRefresh = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarRefreshRate = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox.SuspendLayout();
             this.contextMenuStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRefreshRate)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox
@@ -50,7 +56,7 @@
             this.groupBox.Controls.Add(this.listViewSerial);
             this.groupBox.Location = new System.Drawing.Point(12, 12);
             this.groupBox.Name = "groupBox";
-            this.groupBox.Size = new System.Drawing.Size(297, 397);
+            this.groupBox.Size = new System.Drawing.Size(305, 397);
             this.groupBox.TabIndex = 0;
             this.groupBox.TabStop = false;
             this.groupBox.Text = "List of serial devices";
@@ -61,7 +67,7 @@
             this.listViewSerial.HideSelection = false;
             this.listViewSerial.Location = new System.Drawing.Point(6, 19);
             this.listViewSerial.Name = "listViewSerial";
-            this.listViewSerial.Size = new System.Drawing.Size(285, 372);
+            this.listViewSerial.Size = new System.Drawing.Size(293, 372);
             this.listViewSerial.TabIndex = 0;
             this.listViewSerial.UseCompatibleStateImageBehavior = false;
             this.listViewSerial.View = System.Windows.Forms.View.List;
@@ -70,27 +76,27 @@
             // 
             this.buttonCheckAll.Location = new System.Drawing.Point(18, 415);
             this.buttonCheckAll.Name = "buttonCheckAll";
-            this.buttonCheckAll.Size = new System.Drawing.Size(60, 23);
+            this.buttonCheckAll.Size = new System.Drawing.Size(60, 22);
             this.buttonCheckAll.TabIndex = 1;
-            this.buttonCheckAll.Text = "Check all";
+            this.buttonCheckAll.Text = "All";
             this.buttonCheckAll.UseVisualStyleBackColor = true;
             this.buttonCheckAll.Click += new System.EventHandler(this.buttonCheckAll_Click);
             // 
             // buttonRefresh
             // 
-            this.buttonRefresh.Location = new System.Drawing.Point(150, 415);
+            this.buttonRefresh.Location = new System.Drawing.Point(122, 415);
             this.buttonRefresh.Name = "buttonRefresh";
-            this.buttonRefresh.Size = new System.Drawing.Size(60, 23);
+            this.buttonRefresh.Size = new System.Drawing.Size(80, 22);
             this.buttonRefresh.TabIndex = 3;
-            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.Text = "Refresh now";
             this.buttonRefresh.UseVisualStyleBackColor = true;
             this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
             // 
             // buttonApply
             // 
-            this.buttonApply.Location = new System.Drawing.Point(84, 415);
+            this.buttonApply.Location = new System.Drawing.Point(257, 415);
             this.buttonApply.Name = "buttonApply";
-            this.buttonApply.Size = new System.Drawing.Size(60, 23);
+            this.buttonApply.Size = new System.Drawing.Size(60, 22);
             this.buttonApply.TabIndex = 4;
             this.buttonApply.Text = "Hide";
             this.buttonApply.UseVisualStyleBackColor = true;
@@ -116,32 +122,77 @@
             // toolStripMenuItemRefresh
             // 
             this.toolStripMenuItemRefresh.Name = "toolStripMenuItemRefresh";
-            this.toolStripMenuItemRefresh.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemRefresh.Size = new System.Drawing.Size(113, 22);
             this.toolStripMenuItemRefresh.Text = "Refresh";
             this.toolStripMenuItemRefresh.Click += new System.EventHandler(this.toolStripMenuItemRefresh_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
-            // 
-            // toolStripMenuItemExit
-            // 
-            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
-            this.toolStripMenuItemExit.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItemExit.Text = "Exit";
-            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(110, 6);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(110, 6);
+            // 
+            // toolStripMenuItemExit
+            // 
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(113, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
+            this.toolStripMenuItemExit.Click += new System.EventHandler(this.toolStripMenuItemExit_Click);
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Interval = 60000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "100 ms";
+            // 
+            // trackBarRefreshRate
+            // 
+            this.trackBarRefreshRate.Location = new System.Drawing.Point(18, 464);
+            this.trackBarRefreshRate.Minimum = 1;
+            this.trackBarRefreshRate.Name = "trackBarRefreshRate";
+            this.trackBarRefreshRate.Size = new System.Drawing.Size(291, 45);
+            this.trackBarRefreshRate.TabIndex = 7;
+            this.trackBarRefreshRate.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBarRefreshRate.Value = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 448);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "10 min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Refresh time: ";
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(321, 451);
+            this.ClientSize = new System.Drawing.Size(324, 521);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarRefreshRate);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonApply);
             this.Controls.Add(this.buttonRefresh);
             this.Controls.Add(this.buttonCheckAll);
@@ -151,7 +202,9 @@
             this.Text = "SerialDevices";
             this.groupBox.ResumeLayout(false);
             this.contextMenuStripMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRefreshRate)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -168,6 +221,11 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemExit;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Timer timerRefresh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarRefreshRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
