3cf9c4b86bb4da676a144fc2d68abc1ccf2293ed
juranvrsk <pim1289@yandex.ru>
2024-08-22 20:56:17 +0300

    Upgraded to net 8.0 ToastNotification packet plugged

--- SerialDeviceWidget/SerialDeviceWidget/Form1.cs
+++ SerialDeviceWidget/SerialDeviceWidget/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace SerialDeviceWidget
 {
